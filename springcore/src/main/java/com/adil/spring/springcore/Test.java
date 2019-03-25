557c4a547b04e0614e96e3cb5e3e74966952baa6
adilansari08642 <mohammad.ansari@e-zest.in>
2019-03-25 19:07:32 +0530

    Create and run the test

---
+++ springcore/src/main/java/com/adil/spring/springcore/Test.java
@@ -0,0 +1,13 @@
+package com.adil.spring.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/adil/spring/springcore/config.xml");
+		Employee emp = (Employee) ctx.getBean("emp");
+		System.out.println("Employee ID :"+emp.getId());
+		System.out.println("Employee Name :"+emp.getName());
+	}
+}
