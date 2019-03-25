332cb2a2b9607235fb35771b8636889c848a49a2
adilansari08642 <mohammad.ansari@e-zest.in>
2019-03-25 20:03:41 +0530

    config.xml correct path

--- springcore/src/main/java/com/adil/spring/springcore/Test.java
+++ springcore/src/main/java/com/adil/spring/springcore/Test.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/adil/spring/springcore/config.xml");
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 		Employee emp = (Employee) ctx.getBean("emp");
 		System.out.println("Employee ID :"+emp.getId());
 		System.out.println("Employee Name :"+emp.getName());
