ce8f5a1562bda14ea7550d776af05d2bd88cc7ac
adilansari08642 <mohammad.ansari@e-zest.in>
2019-03-27 15:57:10 +0530

    043: Configure pre shutdown hook

--- springcore/src/main/java/com/adil/spring/springcore/lc/xmlconfig/Test.java
+++ springcore/src/main/java/com/adil/spring/springcore/lc/xmlconfig/Test.java
@@ -1,14 +1,15 @@
 package com.adil.spring.springcore.lc.xmlconfig;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
 				"com/adil/spring/springcore/lc/xmlconfig/config.xml");
 		Patient patient = (Patient) context.getBean("patient");
 		System.out.println(patient);
+		context.registerShutdownHook();
 	}
 }
