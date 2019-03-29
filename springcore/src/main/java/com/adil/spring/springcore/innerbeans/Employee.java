cd756b16a0e911ca72240a2d082f81b8a5121a4e
adilansari08642 <mohammad.ansari@e-zest.in>
2019-03-29 15:52:23 +0530

    051: Inner Beans - Create the bean and config

---
+++ springcore/src/main/java/com/adil/spring/springcore/innerbeans/Employee.java
@@ -0,0 +1,24 @@
+package com.adil.spring.springcore.innerbeans;
+
+public class Employee {
+
+	private int id;
+	private Address address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
