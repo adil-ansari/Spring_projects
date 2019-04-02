ad021ac18cafa87d06996146c48386e2ca382c6c
adilansari08642 <mohammad.ansari@e-zest.in>
2019-04-02 14:54:18 +0530

    060: Ambiguity Problem

---
+++ springcore/src/main/java/com/adil/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -0,0 +1,16 @@
+package com.adil.spring.springcore.constructorinjection.ambiguity;
+
+public class Addition {
+	
+	public Addition(int a,int b) {
+		System.out.println("Inside constructor INT");
+	}
+	
+	public Addition(double a,double b) {
+		System.out.println("Inside constructor DOUBLE");
+	}
+
+	public Addition(String a,String b) {
+		System.out.println("Inside constructor STRING");
+	}
+}
